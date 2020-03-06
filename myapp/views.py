2e54ebcebef47e17e97ed48efb62669445c5c371
unknown <indigo218@naver.com>
2020-03-06 14:10:23 +0900

    200306 next button

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def home(request):
+    return render(request,'home.html')
     screen_id=1
     screen_detail=get_object_or_404(Screen,pk=screen_id)
     next_screen_id=screen_id+1
@@ -18,12 +19,14 @@ def create(request):
 def detail(request,screen_id): 
   #https://tutorial.djangogirls.org/ko/extend_your_application/
   #https://ssungkang.tistory.com/entry/Django-06pk-path-converter-getobjector404%EB%9E%80?category=320582
-  
-  screen_detail=get_object_or_404(Screen,pk=screen_id)
-  next_screen_id=screen_id+1
-  before_screen_id=screen_id-1
-  return render(request,'new.html',{'screen':screen_detail,'next_screen_id':next_screen_id,'before_screen_id':before_screen_id})
-
+  try:
+    screen_detail=get_object_or_404(Screen,pk=screen_id)
+    next_screen_id=screen_id+1
+    before_screen_id=screen_id-1
+    return render(request,'new.html',{'screen':screen_detail,'next_screen_id':next_screen_id,'before_screen_id':before_screen_id})
+  except:
+    return render(request,'home.html')
+    
 def update(request,screen_id):
   print(request.POST['choice'])
   next_screen_id=screen_id+1
